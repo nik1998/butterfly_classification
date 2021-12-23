6e2c93a36301504a0b5d7d5eda65458f18db53dc
nik1998 <beregnovnikita@gmail.com>
2021-12-23 14:41:34 +0300

    commit pretrained model

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import cv2
 import numpy as np
 import os
 import pandas as pd
+from keras.utils.np_utils import to_categorical
 
 
 def get_labels(filename):
     df = pd.read_csv(filename)
-    for col_name, data in df.items():
-        print("col_name:", col_name, "\ndata:", data)
+    return to_categorical(df['label'].tolist())
+
+
+def save_predictions(lst):
+    df = pd.DataFrame(lst, columns=['results'])
+    df.to_csv("res.csv", index=False)
 
 
 def read_image(imageName, height=0, width=0):
@@ -22,7 +27,7 @@ def read_image(imageName, height=0, width=0):
 def read_dir(imagePath, height, width, sort=False):
     dir = os.listdir(imagePath)
     if sort:
-        dir = sorted(dir)
+        dir = sorted(dir, key=lambda x: int(x[:-4]))
     dir_images = []
     for l in dir:
         dir_images.append(read_image(os.path.join(imagePath, l), height, width))
@@ -34,27 +39,35 @@ def get_model():
         include_top=False,
         weights="imagenet",
         input_shape=(224, 224, 3),
-        classes=1000,
+        #classes=1000,
     )
     inputs = keras.Input(shape=(224, 224, 3))
     x = base_model(inputs)
-    x = keras.layers.GlobalAveragePooling2D()(x)
-    outputs = keras.layers.Dense(50, activation="softmax")(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    outputs = tf.keras.layers.Dense(50, activation="softmax")(x)
     model = keras.Model(inputs, outputs)
-    model.compile(optimizer=keras.optimizers.Adam(1e-5),  # Very low learning rate
-                  loss=keras.losses.CategoricalCrossentropy(),
-                  metrics=[keras.metrics.Accuracy()])
+    model.compile(optimizer=tf.keras.optimizers.Adam(1e-5),  # Very low learning rate
+                  loss=tf.keras.losses.CategoricalCrossentropy(),
+                  metrics="accuracy")
     return model
 
 
-train_x, names = read_dir("final_data/train", 224, 224)
+train_x, names = read_dir("final_data/train", 224, 224, True)
 
-val_x, vnames = read_dir("final_data/valid", 224, 224)
+val_x, vnames = read_dir("final_data/valid", 224, 224, True)
 
 train_y = get_labels("final_data/train_labels.csv")
 val_y = get_labels("final_data/valid_labels.csv")
-# model.fit(new_dataset, epochs=20, callbacks=..., validation_data=...)
 
+model = get_model()
+#model.fit(x=train_x, y=train_y, epochs=20, batch_size=16, validation_data=(val_x, val_y))
+#model.save_weights("weights.h5")
+model.load_weights("weights.h5")
 
+test_x, _ = read_dir("final_data/test", 224, 224, True)
+
+results = model.predict(test_x)
+results = np.argmax(results, axis=1)
+save_predictions(results)
 if __name__ == "__main__":
     pass
